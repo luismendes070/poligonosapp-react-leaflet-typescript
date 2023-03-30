d8c8fdfcf4e1138664e8ab1e7883c62427ef3d1b
luismendes070 <machadoluismendes@gmail.com>
2023-03-30 07:08:57 -0300

    1 file modified

--- index.tsx
+++ index.tsx
@@ -4,11 +4,30 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-const rootElement:any = document.getElementById('root');
-const root:any = createRoot(rootElement);
+try{
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+  const rootElement:any = document.getElementById('root');
+  const root:any = createRoot(rootElement);
+  
+  document.addEventListener("DOMContentLoaded", function(event) {
+    var script = document.createElement("script");
+    document.body.appendChild(script);
+  });
+  
+  
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+
+}catch(e:any){
+
+  console.log("\n src/index exception.");;
+  console.log(e.message);
+
+}finally{
+
+  console.log('src/index reactJS TypeScript finally.');
+
+}
