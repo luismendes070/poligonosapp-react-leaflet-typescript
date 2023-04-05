ca31f2bc052d29ee2a64ef469c76a05dcb8e78ea
luismendes070 <machadoluismendes@gmail.com>
2023-04-05 19:31:09 -0300

    2 files modified, 2 added

---
+++ prod.config.js
@@ -0,0 +1,29 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+// in case you run into any typescript error when configuring `devServer`
+import 'webpack-dev-server';
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './foo.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'foo.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
+
+export default config;
